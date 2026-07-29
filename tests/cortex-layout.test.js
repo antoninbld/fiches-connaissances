c0f6de8efd65ceb4ca13ca49f171dca98823962b
antoninbld <antonin.bouldoires@gmail.com>
2026-07-29 17:03:01 +0200

    Dézoomer la vue initiale du Cortex

--- tests/cortex-layout.test.js
+++ tests/cortex-layout.test.js
@@ -13,5 +13,6 @@ assert.match(html, /const cloudRadiusX=width\*0\.46/, 'le nuage doit exploiter l
 assert.match(html, /const cloudRadiusY=height\*0\.43/, 'le nuage doit exploiter la hauteur disponible');
 assert.match(html, /organicWave=1\+Math\.sin/, 'la distribution doit conserver une irrégularité organique');
 assert.doesNotMatch(html, /sphereRadius=Math\.min\(width,height\)/, 'le Cortex ne doit plus être contraint par une petite sphère centrale');
+assert.match(html, /const CORTEX_VIEW_DEFAULTS=\{panX:0,panY:0,zoom:0\.75\}/, 'la vue initiale doit être suffisamment dézoomée pour montrer le Cortex dans sa globalité');
 
 console.log('Cortex layout: tests réussis');
