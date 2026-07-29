eeefc7515a41c96de26b36e7a149e6e9022fd81f
antoninbld <antonin.bouldoires@gmail.com>
2026-07-29 16:55:42 +0200

    Étendre la distribution organique du Cortex

--- tests/cortex-layout.test.js
+++ tests/cortex-layout.test.js
@@ -9,5 +9,9 @@ assert.ok(layoutPosition >= 0, 'la visualisation du Cortex doit être présente'
 assert.ok(sizeToolbarPosition > layoutPosition, 'le réglage de taille doit apparaître sous la visualisation');
 assert.doesNotMatch(html, /Glisser: orbite 3D/, 'les instructions de navigation ne doivent plus encombrer le graphe');
 assert.match(html, /class="cortex-zoom-controls"/, 'les contrôles de zoom doivent rester disponibles');
+assert.match(html, /const cloudRadiusX=width\*0\.46/, 'le nuage doit exploiter la largeur disponible');
+assert.match(html, /const cloudRadiusY=height\*0\.43/, 'le nuage doit exploiter la hauteur disponible');
+assert.match(html, /organicWave=1\+Math\.sin/, 'la distribution doit conserver une irrégularité organique');
+assert.doesNotMatch(html, /sphereRadius=Math\.min\(width,height\)/, 'le Cortex ne doit plus être contraint par une petite sphère centrale');
 
 console.log('Cortex layout: tests réussis');
